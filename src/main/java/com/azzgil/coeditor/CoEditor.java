3944592d26788ef8bfe0c6a5352e24dc6e2462d4
Sergey Medelyan <fakeAcc18@yandex.ru>
2018-05-20 18:11:29 +0300

    initial commit

---
+++ src/main/java/com/azzgil/coeditor/CoEditor.java
@@ -0,0 +1,12 @@
+package com.azzgil.coeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoEditor {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoEditor.class, args);
+    }
+}
---
+++ src/main/resources/static/main.js
@@ -0,0 +1,6 @@
+new Vue({
+   el: "#main",
+   data: {
+       message: "Hello there, you are about to use CoEditor!"
+   }
+});
